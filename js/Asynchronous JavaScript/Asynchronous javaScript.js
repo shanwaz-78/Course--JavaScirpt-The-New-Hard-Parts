6786a62b6dc694f5822899c0bfda699a3dd918e6
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-04-26 16:34:05 +0530

    Completed Course

--- js/Asynchronous JavaScript/Asynchronous javaScript.js
+++ js/Asynchronous JavaScript/Asynchronous javaScript.js
@@ -13,3 +13,29 @@ function blockFor1Sec() {
 setTimeout(printHello1, 0);
 blockFor1Sec();
 console.log("Me First");
+
+// Initial work.
+async function postData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "Suzy",
+        company: "Amazon",
+        designation: "Developer",
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error in Fetching`);
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+postData();
